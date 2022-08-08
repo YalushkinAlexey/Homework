0d21a93603718dd83a9ad5768b303550ecf1128e
YalushkinAlexey <frostbyte@bk.ru>
2022-08-08 18:35:10 +0300

    добавил программку по массивам

---
+++ Little progs/program010_Elochka/Program.cs
@@ -0,0 +1,24 @@
+﻿// Elochka 
+// Developers by Konkov M.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        int n = 5, m = 9;                             // Размерность массива
+        int[ , ] arr = new int[n , m];                  // инициализация массива
+
+        for (int i = 0; i < n; i++)                     // запускаем внешний цикл  ось OY
+        {
+            for (int j = 0; j < m; j++)                 // запускаем внутренний цикл ось ОХ
+            {
+                if (j >= m/2 - i && j <= m/2 + i)       // Условие заполнения елочки
+                arr[i , j] = 1;                         // в ячейку с определенным индексом (по условию) закидываем значение
+                
+                Console.Write(arr[i , j] + " ");        // выводим в терминал
+            }
+            Console.WriteLine(" ");                     // этот нужен чтобы перескочить на новую строку
+        }
+    }
+}
