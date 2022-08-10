5cb34c3cade96851ce7db9fa8d24c30a96c9ef60
YalushkinAlexey <frostbyte@bk.ru>
2022-08-10 22:45:45 +0300

    методы и функции

---
+++ Little progs/methods/Program.cs
@@ -0,0 +1,30 @@
+﻿public class CustomFuctions
+{
+    private static void Main(string[] args) 
+    {
+
+    void FillArray(int[] col)
+    {
+        int length = col.Length;
+        int index = 0;
+        while (index < length)
+        {
+            col[index] = new Random().Next(1, 10);
+            index++;
+        }
+    }
+
+    void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+        {
+            Console.WriteLine(col[position]);
+            count++;
+        }
+    }
+
+    Console.WriteLine("методы FillArray PrintArray");
+    }
+}
