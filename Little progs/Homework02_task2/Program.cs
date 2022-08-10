c944b2cb242e733ae95a883eceba5f985acb78bc
YalushkinAlexey <frostbyte@bk.ru>
2022-08-10 12:06:20 +0300

    третья цифра из введенных в консоль символов

---
+++ Little progs/Homework02_task2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+string str = Convert.ToString(Console.ReadLine());
+int i = 0;
+foreach (char c in str)
+{
+    if (char.IsDigit(c))
+    {
+        i++;
+    }
+    if (i == 3)
+    {
+        Console.WriteLine("Третья цифра : " + c);
+        return;
+    }
+}
+Console.WriteLine("третьей цифры нет");
