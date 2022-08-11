d25abac093dc24609ae7613312faa653ca6951e7
YalushkinAlexey <frostbyte@bk.ru>
2022-08-11 12:45:39 +0300

    Анаграмма

---
+++ Little progs/program014_anagram/Program.cs
@@ -0,0 +1,19 @@
+﻿// Анаграммы
+
+Console.Clear();
+Console.WriteLine("введите 2 слова, ");
+string str = Convert.ToString(Console.ReadLine()).ToLower();
+string str2 = Convert.ToString(Console.ReadLine()).ToLower();
+bool flag = true;
+if (str.Length != str2.Length){Console.WriteLine("не анаграмма");}
+else
+{           // КОТ   -  ТОК
+    foreach(char c in str)
+    {
+        if (!str2.Contains(c))
+        {
+            flag = false;
+        }
+    }
+    Console.WriteLine(flag == true ? "да, это анаграмма": "не анаграмма" );
+}
