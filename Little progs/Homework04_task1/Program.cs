772fe9855a8291491007206dbdff68a38c1c60ca
YalushkinAlexey <frostbyte@bk.ru>
2022-08-16 11:51:10 +0300

    4 домашняя работа

---
+++ Little progs/Homework04_task1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.Clear();
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PowerUp(a, b) + " и для сравнения Math.Pow " + Math.Pow(a, b));
+
+int PowerUp(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
---
+++ Little progs/Homework04_task3/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.Clear();
+string text = " Напишите программу, которая задаёт массив из (n) элементов и выводит их на экран.";
+Console.WriteLine(text);
+Console.Write("\n размерность массива - ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 0)
+{
+    Console.WriteLine(" Массив должен иметь длину больше 0 ");
+}
+else
+    Massivchik(n);
+
+void Massivchik(int n)
+{
+    int[] array = new int[n];
+    Console.Write("[");
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    Console.Write(string.Join(", ", array));
+    Console.Write("]");
+}
