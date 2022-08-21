1fa9e1ae66cc5080d39bf5e88371d5bff9d87d0b
YalushkinAlexey <frostbyte@bk.ru>
2022-08-21 22:52:56 +0300

    try catch

---
+++ Little progs/program015_tryCatch/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+
+Метод int.TryParse(string s, out int result) проверяет, можно ли получить число из строки. 
+Если это возможно - возвращает true и полученное число out-параметром, 
+иначе возвращает false результатом и default(int) out-параметром.
+
+Try  - Catch
+TryParse           
+*/
+
+Console.Clear();
+int TypoText()
+{
+    string stroka = "123";
+    bool text = int.TryParse(stroka, out int result);
+    return result;
+}
+
+int result = TypoText();
+
+try
+{
+    object str = Console.ReadLine();
+    int num = Convert.ToInt32(str);
+    Console.WriteLine(num*2 + " " + result);
+}
+catch (Exception e)
+{
+    Console.WriteLine("так не пойдет");
+}
+
